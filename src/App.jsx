73fd7bb966efaad9ac402da2d956c425be49c2f7
Hugo Bonifay <hugobonifay.dev@gmail.com>
2025-01-26 19:09:50 +0100

    add scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,44 @@
 import "./App.css";
 import Blockchain from "./components/Blockchain";
 import Nav from "./components/Nav";
-import { Route, Routes } from "react-router";
+import { Outlet, Route, Routes, useLocation } from "react-router";
 import Web3 from "./components/Web3";
 import SmartContracts from "./components/SmartContracts";
 import Solidity from "./components/Solidity";
 import Dapp from "./components/Dapp";
 import CryptoWallets from "./components/CryptoWallets";
+import { useEffect } from "react";
 
-const Routing = () => {
+const Layout = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
-    <Routes>
-      <Route index path="/web3/" element={<Web3 />} />
-      <Route path="/web3/blockchain/" element={<Blockchain />} />
-      <Route path="/web3/smart-contracts/" element={<SmartContracts />} />
-      <Route path="/web3/solidity/" element={<Solidity />} />
-      <Route path="/web3/dapp/" element={<Dapp />} />
-      <Route path="/web3/crypto-wallets/" element={<CryptoWallets />} />
-    </Routes>
+    <>
+      <Nav />
+      <main>
+        <Outlet />
+      </main>
+    </>
   );
 };
 
-function App() {
+const App = () => {
   return (
-    <div
-      style={{
-        width: "90%",
-        maxWidth: "600px",
-        margin: "auto",
-        padding: "75px 0 75px 0",
-      }}
-    >
-      <Nav />
-      <Routing />
-    </div>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route index path="/web3/" element={<Web3 />} />
+        <Route path="/web3/blockchain/" element={<Blockchain />} />
+        <Route path="/web3/smart-contracts/" element={<SmartContracts />} />
+        <Route path="/web3/solidity/" element={<Solidity />} />
+        <Route path="/web3/dapp/" element={<Dapp />} />
+        <Route path="/web3/crypto-wallets/" element={<CryptoWallets />} />
+      </Route>
+    </Routes>
   );
-}
+};
 
 export default App;
